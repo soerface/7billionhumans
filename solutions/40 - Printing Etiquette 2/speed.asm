5b7edd0e9176f1e4c4fb08150a138e2f2eabce85
Soeren Wegener <wegener92@gmail.com>
2018-09-02 00:14:58 +0200

    Add 40 - Printing Etiquette 2

---
+++ solutions/40 - Printing Etiquette 2/speed.asm
@@ -0,0 +1,29 @@
+-- 7 Billion Humans (2053) --
+-- 40: Printing Etiquette 2 --
+
+-- Target Size: 10
+-- Size: 17
+-- Target Speed: 55
+-- Speed: 55
+
+mem1 = nearest printer
+takefrom mem1
+step w
+write 5
+a:
+if c == datacube:
+	step w
+	jump a
+endif
+b:
+drop
+if mem2 == 1:
+	end
+endif
+mem2 = nearest datacube
+takefrom mem1
+step mem2
+mem2 = calc mem2 - 1
+write mem2
+step n
+jump b
