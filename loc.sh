6f8fe2e26b8fe5da90d7be542ee1d4faaf47615a
Anselm von Wangenheim <anselm.w@gmx.de>
2018-09-07 01:23:21 +0200

    omit question for size

--- loc.sh
+++ loc.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-grep -vP "^\t*(--.*|\w{1,3}:|(end|comment).+|.*(and|or))?$" "$1" | grep -v "^$" | sed '/^\DEFINE COMMENT.*/,$ d' | wc -l
+if [ -z "$1" ]; then
+	cat
+else
+	cat "$1"
+fi | grep -vP "^\t*(--.*|\w{1,3}:|(end|comment).+|.*(and|or))?$" | grep -v "^$" | sed '/^\DEFINE COMMENT.*/,$ d' | wc -l
 
 # test with
 # for f in */*/*.asm; do echo $f; if [ $(./loc.sh "$f") != $(grep "^-- Size:" "$f" | cut -c10-) ] ; then echo "error at $f"; fi; done
