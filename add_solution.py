aee37497d60992fbeb2694c4b738efe633a12866
Soeren Wegener <wegener92@gmail.com>
2018-09-08 02:04:33 +0200

    Delete old solutions if new solutions dominate old ones

--- add_solution.py
+++ add_solution.py
@@ -55,15 +55,31 @@ def get_score(path, category):
     return math.inf
 
 
-def save(source, path):
+def save(source, dirname, size, speed):
+
+    path = dirname / f'size-{size}_speed-{speed}.asm'
+
     if path.exists():
         print('Already exists, not overwriting')
         return
-    if not path.parent.exists():
-        path.parent.mkdir()
+    if not dirname.exists():
+        dirname.mkdir()
+
+    other_scores = [(get_score(path, 'size'), get_score(path, 'speed'), path) for path in dirname.glob('*.asm')]
+
+    if any((other_size <= size and other_speed <= speed for other_size, other_speed, _ in other_scores)):
+        print('Your solution is dominated by another solution, not saving')
+        return
+
     with path.open('w') as f:
         f.write('\n'.join(source))
+    subprocess.call(['git', 'add', path])
     print(f'Saved:                                      {str(path):>40}')
+
+    for other_size, other_speed, path in other_scores:
+        if size <= other_size and speed <= other_speed:
+            print(f'Removed:                                     {str(path):>40}')
+            subprocess.call(['git', 'rm', path])
 
 
 def add_solution():
@@ -81,12 +97,7 @@ def add_solution():
     print()
     dirname = f'{level_id:02} - {level_name}'
 
-    other_scores = [(get_score(path, 'size'), get_score(path, 'speed')) for path in (BASE_PATH / dirname).glob('*.asm')]
-
-    if any((other_size <= size and other_speed <= speed for other_size, other_speed in other_scores)):
-        print('Your solution is dominated by another solution')
-    else:
-        save(source, BASE_PATH / dirname / f'size-{size}_speed-{speed}.asm')
+    save(source, BASE_PATH / dirname, size, speed)
     print()
 
     return input('Add another source? [y/N] ').lower() == 'y'
