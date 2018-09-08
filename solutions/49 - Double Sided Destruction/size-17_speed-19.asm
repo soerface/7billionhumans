78074f0b25b521f495ade1b1dd9b9e03064ce049
Soeren Wegener <wegener92@gmail.com>
2018-09-09 01:37:11 +0200

    OK GO!

---
+++ solutions/49 - Double Sided Destruction/size-17_speed-19.asm
@@ -0,0 +1,26 @@
+-- 7 Billion Humans (2053) --
+-- 49: Double Sided Destruction --
+
+-- Size: 17
+-- Speed: 19
+
+mem1 = nearest shredder
+pickup s
+a:
+if w != worker:
+	giveto mem1
+	step sw,se
+	tell everyone ok
+	step sw,s,se
+	end
+endif
+if e != worker:
+	giveto mem1
+	step sw,se
+	tell everyone go
+	step sw,s,se
+	end
+endif
+listenfor ok
+listenfor go
+jump a
