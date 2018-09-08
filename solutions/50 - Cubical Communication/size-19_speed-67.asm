d2e6e9f73e9af3ff035ddc2c031aea5a07f1d9cc
Soeren Wegener <wegener92@gmail.com>
2018-09-09 01:58:17 +0200

    Add 50 - Cubical Communication

---
+++ solutions/50 - Cubical Communication/size-19_speed-67.asm
@@ -0,0 +1,30 @@
+-- 7 Billion Humans (2053) --
+-- 50: Cubical Communication --
+
+-- Size: 19
+-- Speed: 67
+
+if sw == 1:
+	jump a
+endif
+if sw == 2:
+	jump b
+endif
+if sw != 3:
+	listenfor ok
+endif
+listenfor ok
+b:
+listenfor ok
+a:
+takefrom s
+giveto se
+takefrom sw
+giveto se
+takefrom sw
+giveto se
+takefrom sw
+giveto se
+takefrom sw
+giveto se
+tell everyone ok
