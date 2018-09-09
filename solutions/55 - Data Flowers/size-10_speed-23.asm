0421fd52aec18b9442b91f9295486c436bba4ebf
Soeren Wegener <wegener92@gmail.com>
2018-09-09 04:11:32 +0200

    Add 55 - Data Flowers

---
+++ solutions/55 - Data Flowers/size-10_speed-23.asm
@@ -0,0 +1,20 @@
+-- 7 Billion Humans (2053) --
+-- 55: Data Flowers --
+
+-- Size: 10
+-- Speed: 23
+
+pickup s
+step n
+step n
+a:
+step n
+if c != nothing or
+ s != datacube:
+	jump a
+endif
+mem1 = foreachdir nw,w,sw,n,s,ne,e,se:
+	mem2 = calc mem1 + mem2
+endfor
+write mem2
+drop
