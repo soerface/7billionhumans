fb65e16cbe8fe26a8578d6af63205baf7f1bec2d
Soeren Wegener <wegener92@gmail.com>
2018-09-09 02:06:36 +0200

    Add 52 - Identify Yourselves

---
+++ solutions/51 - Identify Yourselves/size-24_speed-19.asm
@@ -0,0 +1,35 @@
+-- 7 Billion Humans (2053) --
+-- 51: Identify Yourselves --
+
+-- Size: 24
+-- Speed: 19
+
+step s
+pickup c
+if w == nothing:
+	write 1
+	drop
+	end
+else:
+	if e == nothing:
+		write 10
+		drop
+		end
+	else:
+		write 0
+	endif
+endif
+a:
+if w != 0:
+	mem1 = calc w + 1
+	write mem1
+	drop
+	end
+endif
+if e != 0:
+	mem1 = calc e - 1
+	write mem1
+	drop
+	end
+endif
+jump a
