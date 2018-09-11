807e8c9939e8381d7b0972e69feb9379ff13dc61
Soeren Wegener <wegener92@gmail.com>
2018-09-11 21:44:33 +0200

    Add very fast solution for 09 - Dynamic Angles

---
+++ solutions/09 - Dynamic Angles/size-18_speed-3.asm
@@ -0,0 +1,37 @@
+-- 7 Billion Humans (2053) --
+-- 9: Dynamic Angles --
+
+-- Author: soerface
+-- Size: 18
+-- Speed: 3
+
+step s
+pickup c
+if e == nothing:
+	jump a
+endif
+if e == nothing:
+	jump b
+endif
+if e == nothing:
+	jump c
+endif
+if e == nothing:
+	jump d
+endif
+if e == nothing:
+	jump e
+endif
+a:
+step s
+b:
+step s
+c:
+step s
+d:
+step s
+e:
+step s
+drop
+
+
