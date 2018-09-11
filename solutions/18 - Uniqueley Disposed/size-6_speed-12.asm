de25d3024851f7746dce55078b631706b6fc1fde
lucy phipps <landfillbaby@users.noreply.github.com>
2018-09-12 00:45:06 +0100

    18 - size 6, speed 12, no deaths

--- solutions/18 - Uniqueley Disposed/size-6_speed-12.asm
+++ solutions/18 - Uniqueley Disposed/size-6_speed-12.asm
@@ -1,24 +1,15 @@
--- 7 Billion Humans (2053) --
+-- 7 Billion Humans (2087) --
 -- 18: Uniquely Disposed --
 
--- Author: soerface
+-- Author: landfillbaby
 -- Size: 6
 -- Speed: 12
 
 pickup w
 step sw
 a:
-if s == shredder:
-	giveto s
-	comment 0
+if s == worker:
+	step se
+	jump a
 endif
-step se
-jump a
-
-
-DEFINE COMMENT 0
-eJztjbFqwgAURc+3tNLSRVspFTRKBnEobSziFERMKXVzKhlCWy7SgkiHkiG42Do4SMjQD8jH+ZK5n5AL
-l/e4nHdfG9ApN6rRp8bGZsoJB4r8jEs1uJZDnS7NIuOciCsitQjVISyzCzI1SeWyL+88vnXPk/lWQ/p6
-wNEIVz4TpiwUWMcj7zbXmvFi9gkYMzd2btwzvbIn4o43I5fGLknKX2Jg+0Af1v2Fp5ixfqxjx8oc65dE
-W2M/yY3LeeXPOjIqVar0r46GRTpa;
-
+giveto s
