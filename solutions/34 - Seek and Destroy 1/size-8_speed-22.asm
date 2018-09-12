f8b6a40b57a118b2569819a35a659a3b9adbb8a3
lucy phipps <landfillbaby@users.noreply.github.com>
2018-09-12 16:44:26 +0100

    34 -  size 8, speed 22

--- solutions/34 - Seek and Destroy 1/size-8_speed-22.asm
+++ solutions/34 - Seek and Destroy 1/size-8_speed-22.asm
@@ -1,15 +1,15 @@
--- 7 Billion Humans (2053) --
+-- 7 Billion Humans (2087) --
 -- 34: Seek and Destroy 1 --
 
--- Author: soerface
--- Size: 9
+-- Author: landfillbaby
+-- Size: 8
 -- Speed: 22
 
-mem1 = set 99
 mem2 = nearest shredder
 a:
 step n
-if c < mem1 and
+if c < mem1 or
+ mem1 != datacube and
  c == datacube:
 	mem1 = set c
 endif
