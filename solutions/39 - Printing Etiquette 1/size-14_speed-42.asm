e36ae853c6a20a1b4ad9df6570925b947b8d0dc4
lucy phipps <landfillbaby@users.noreply.github.com>
2018-09-13 00:20:15 +0100

    39 - size 14, speed 42

---
+++ solutions/39 - Printing Etiquette 1/size-14_speed-42.asm
@@ -0,0 +1,25 @@
+-- 7 Billion Humans (2087) --
+-- 39: Printing Etiquette 1 --
+
+-- Author: landfillbaby
+-- Size: 14
+-- Speed: 42
+
+mem1 = nearest printer
+takefrom mem1
+step sw
+step w
+a:
+if c == datacube:
+	step n
+	jump a
+endif
+b:
+drop
+if w != wall:
+	mem2 = nearest datacube
+	takefrom mem1
+	step mem2
+	step w
+	jump b
+endif
