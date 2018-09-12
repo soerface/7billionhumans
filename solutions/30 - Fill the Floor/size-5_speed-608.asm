e37688a4125c8847a87e3f63325b00c3714c89c1
lucy phipps <landfillbaby@users.noreply.github.com>
2018-09-12 16:24:35 +0100

    30 - size 5, speed 608

---
+++ solutions/30 - Fill the Floor/size-5_speed-608.asm
@@ -0,0 +1,13 @@
+-- 7 Billion Humans (2087) --
+-- 30: Fill the Floor --
+
+-- Author: landfillbaby
+-- Size: 5
+-- Speed: 608
+
+mem1 = nearest printer
+a:
+takefrom mem1
+step nw,w,sw,s,ne,e,se
+drop
+jump a
