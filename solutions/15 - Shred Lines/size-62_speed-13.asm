6fafc8412abe729171171668007a340bfb2cb072
lucy phipps <landfillbaby@users.noreply.github.com>
2018-09-12 11:43:00 +0100

    15 - size 62, speed 13

---
+++ solutions/15 - Shred Lines/size-62_speed-13.asm
@@ -0,0 +1,122 @@
+-- 7 Billion Humans (2087) --
+-- 15: Shred Lines --
+
+-- Author: landfillbaby
+-- Size: 62
+-- Speed: 13
+
+step n
+if n == datacube:
+	comment 1
+	pickup n
+	step s
+	giveto s
+	step n
+	step n
+	if n == datacube:
+		comment 0
+		pickup n
+		step s
+		step s
+		giveto s
+		step n
+		step n
+		step n
+		step n
+		pickup n
+		jump a
+	endif
+	comment 2
+	step n
+	pickup n
+	step s
+	step s
+	step s
+	giveto s
+	step n
+	step n
+	step n
+	step n
+	if n == datacube:
+		comment 4
+		pickup n
+		jump b
+	endif
+	comment 5
+	jump c
+endif
+comment 3
+step n
+pickup n
+step s
+step s
+giveto s
+step n
+step n
+step n
+if n == datacube:
+	comment 6
+	pickup n
+else:
+	comment 7
+	step n
+	pickup n
+	step s
+endif
+step s
+step s
+step s
+giveto s
+step n
+step n
+step n
+step n
+c:
+step n
+pickup n
+step s
+a:
+b:
+step s
+step s
+step s
+step s
+giveto s
+
+
+DEFINE COMMENT 0
+eJyTYQACMYYaENWgAaEZ7BiqwHQMQ1RDFEMSQwRDTUMIQw9DCMOkhgCGOQwBDIsa/BiWgfV4AlX5MuQA
+xYoaAhmqgOoaGsIZuhgiGaYA8QyGWIYlDXEMa8DmZTPUMYyCUTAKBhUAABK6F1s;
+
+DEFINE COMMENT 1
+eJztjjtKg1EUhL8FWKhYCD5IEwvFDbiC+AqiIBIUQkiRwkCsVPjRUX5FY4gvCCIiKkEsUgQRkSBBxMLC
+RTm5uArJwHAvzDfnnBSgJIskyGiUnIYpMESJQcr2rQZo0s+b+mirlzYdvsfsiPMEFZJca4wHjdNggqYm
+eQnMFFF415hhlRUynr3Enf3IAq9K27N8OP3UNN+BnSOrebKkyfuidYp8qeRsg59OziXLOiPHgXfH3LBP
+nV3fFvGu7T9mk7z/BXbYkoi0ZyrmUDFlHXHBMTVOuFLF/ar7VZ50GtzQOc8mWt7TCrPu3eyqq3+uX7XL
+TAQ;
+
+DEFINE COMMENT 2
+eJztjT0LQQEYRs/uIxmVkgysZj9APhfk8xbdkk0GGaRnMhiMkskg+Q0Gg8EgyeAHed38AvM9dbbnPW8W
+UIa20jiKsSDKlghHApzNC1+CjAkxMacKM1eUJXF2JDgoaaY4fWfKIW/vkjcrDGiob92e3Xet3WJNw/p1
+DtQ4qspZZS4qcTVvKvKgwMtrFe2uidRhJYe9XNsNuWvE03x7f2a/fxtr+/j4/MUHZ3Iviw;
+
+DEFINE COMMENT 3
+eJxTZAACYYY6ENVgzeDTYAXElgwBDGYMYQxGDFkNugwdDToM0xo0GFYxqDPsAqljUGWIYNBmaGDQZ5jE
+YMWwicGGYQtYvw9DB1g+AUpXAlmlQDX5DMcYshnugNWkMwQ0FDNMaShlWMJQz3CooYHhFMMoGAWjYMAA
+AOiLG1o;
+
+DEFINE COMMENT 4
+eJyTYwACcYaqBlGGugYRhhYGYYYOBkGGKQz8DLOAeAEDHxgvaeBiWNfAwbADpBxI1zBwM3Q18DD0NPAC
+1QowzGEQYlgB1L+GQZRhA9C8LSB1DMoMk8C0PlANCNgAzQTp94CKj4JRMAoGHAAAitEU+w;
+
+DEFINE COMMENT 5
+eJyTYgACGYYOENWgCqXNGEqAuILBlKEBiNsYTBi6GowZpjXoMmwDy+swFABxVYMeQ1uDIcMEIJ4ClJ8B
+VLugwZxhSYMFwwqwOmeGLjDtB6FHwSgYBYMPAADEtBaa;
+
+DEFINE COMMENT 6
+eJyTZAACWYY2EMVgyZDCYMZQxKDDsIdBk+EMgzrDhQY1hitgOU2GrAYtoJwRQ1eDEcOEBlOGWUD1qxqc
+GI41ODMcY3BhOAVW58dQB6YTGLrAdB5DC8MoGAWjYNACAEz4EvY;
+
+DEFINE COMMENT 7
+eJwTYwACcYYuENWgBKEZtKF8YyjtzFDQ4MhQxuDAUNdgw7CswYphA4MVwz6wnBlDEoMlQw6QXwaUq2Fw
+ZOhpcGdYwzAKRsEoGBIAAItVEKc;
