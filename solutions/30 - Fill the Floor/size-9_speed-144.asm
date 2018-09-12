103125ef357e6ea8f08c8ed1ebfc30861de12424
lucy phipps <landfillbaby@users.noreply.github.com>
2018-09-12 16:07:33 +0100

    30 - size 9, speed 144

---
+++ solutions/30 - Fill the Floor/size-9_speed-144.asm
@@ -0,0 +1,19 @@
+-- 7 Billion Humans (2087) --
+-- 30: Fill the Floor --
+
+-- Author: landfillbaby
+-- Size: 9
+-- Speed: 144
+
+mem1 = nearest printer
+a:
+mem2 = nearest wall
+takefrom mem1
+step mem2
+b:
+if c == datacube:
+	step nw,w,sw,n,s,ne,e,se
+	jump b
+endif
+drop
+jump a
