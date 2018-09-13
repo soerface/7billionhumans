cc35eed35742a87e08a2f56254f5f3ba284ddf49
田生 <tiansheng111@gmail.com>
2018-09-14 00:18:32 +1000

    Level 51

---
+++ solutions/51 - Identify Yourselves/size-7_speed-83.asm
@@ -0,0 +1,18 @@
+-- 7 Billion Humans (2056) --
+-- 51: Identify Yourselves --
+
+-- Author: tiansh
+-- Size: 7
+-- Speed: 83
+
+a:
+if w != worker:
+	mem1 = calc w + 1
+	pickup c
+	write mem1
+	drop
+	step s
+endif
+jump a
+
+
