27387cfd3f9c4cc03376775b890b7f8498ed04bd
田生 <tiansheng111@gmail.com>
2018-09-14 00:22:06 +1000

    Fix path parameter for git rm also

--- add_solution.py
+++ add_solution.py
@@ -84,7 +84,7 @@ def save(source, dirname, size, speed):
     for other_size, other_speed, path in other_scores:
         if size <= other_size and speed <= other_speed:
             print(f'Removed:                                     {str(path):>40}')
-            subprocess.call(['git', 'rm', '-f', path])
+            subprocess.call(['git', 'rm', '-f', str(path)])
 
 
 def add_solution(author):
