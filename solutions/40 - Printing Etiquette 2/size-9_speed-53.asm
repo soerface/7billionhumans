6701b05aa9da995c6fff453bfd4fbf7ddeb6f96c
Lucy Phipps <landfillbaby@outlook.com>
2018-09-13 01:01:32 +0100

    40 - size 9, speed 53

---
+++ solutions/40 - Printing Etiquette 2/size-9_speed-53.asm
@@ -0,0 +1,21 @@
+-- 7 Billion Humans (2087) --
+-- 40: Printing Etiquette 2 --
+
+-- Author: landfillbaby
+-- Size: 9
+-- Speed: 53
+
+a:
+mem1 = nearest printer
+if mem2 < 5 and
+ myitem == nothing:
+	takefrom mem1
+endif
+step nw,w,sw,n,ne,e,se
+if c == nothing and
+ myitem != nothing:
+	mem2 = calc mem2 + 1
+	write mem2
+	drop
+endif
+jump a
