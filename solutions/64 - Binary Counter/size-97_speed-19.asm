5896deff81e55ea9bbe8a6e1d0b6701cc921abde
Soeren Wegener <wegener92@gmail.com>
2018-09-15 23:01:09 +0200

    Add fast solution for 64 - Binary Counter

---
+++ solutions/64 - Binary Counter/size-97_speed-19.asm
@@ -0,0 +1,116 @@
+-- 7 Billion Humans (2144) --
+-- 64: Binary Counter --
+
+-- Author: soerface
+-- Size: 97
+-- Speed: 19
+
+if w == datacube:
+	step s
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	a:
+	step s
+	step n
+	jump a
+endif
+if e == nothing:
+	step s
+	step s
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	pickup n
+	b:
+	mem1 = set c
+	drop
+	mem1 = set c
+	pickup c
+	jump b
+endif
+if w == nothing:
+	step s
+	step s
+	mem1 = set c
+	mem1 = set c
+	pickup n
+	c:
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	drop
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	pickup c
+	jump c
+endif
+if e == nothing:
+	step s
+	step s
+	mem1 = set c
+	pickup n
+	d:
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	drop
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	pickup c
+	jump d
+endif
+if w == nothing:
+	step s
+	step s
+	pickup n
+	e:
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	drop
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	mem1 = set c
+	pickup c
+	jump e
+endif
+
+
