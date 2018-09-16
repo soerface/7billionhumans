82c6dd4e8630158fff07f0ace1d998a302945ebe
Anselm von Wangenheim <anselm.w@gmx.de>
2018-09-16 19:05:35 +0200

    so fast defrag ordered

---
+++ solutions/65 - Defrag Ordered/size-125_speed-15.asm
@@ -0,0 +1,238 @@
+-- 7 Billion Humans (2053) --
+-- 65: Defrag Ordered --
+
+-- Author: ansvonwa
+-- Size: 125
+-- Speed: 15
+
+step n
+step n
+if c == datacube and
+ ne == datacube:
+	comment 0
+	comment 1
+	pickup c
+	step ne
+	step ne
+	step ne
+	drop
+	step n
+	step nw
+	comment 2
+	pickup e
+	drop
+	step e
+	step e
+	step e
+	comment 4
+	pickup e
+	step w
+	step w
+	drop
+	comment 10
+	step s
+	pickup c
+	step e
+	step e
+	step ne
+	drop
+	step s
+	step s
+	step s
+	step sw
+	pickup sw
+	step n
+	step n
+	drop
+	end
+endif
+if c == datacube and
+ nw == datacube:
+	comment 3
+	step nw
+	comment 5
+	pickup nw
+	step ne
+	step e
+	step e
+	step ne
+	step ne
+	drop
+	comment 13
+	pickup s
+	step w
+	drop
+	step s
+	step s
+	step s
+	pickup s
+	step ne
+	step n
+	drop
+	step w
+	step w
+	pickup c
+	step nw
+	drop
+	step s
+	step s
+	step s
+	pickup sw
+	step ne
+	step n
+	drop
+	end
+endif
+if c != datacube:
+	comment 6
+	step n
+	step ne
+	step ne
+	comment 7
+	pickup c
+	step nw
+	drop
+	comment 11
+	pickup e
+	step w
+	step w
+	step w
+	step w
+	step w
+	drop
+	comment 14
+	pickup s
+	step e
+	drop
+	step e
+	pickup s
+	drop
+	step e
+	pickup s
+	drop
+	step s
+	pickup s
+	step w
+	step w
+	step w
+	drop
+	end
+endif
+comment 8
+step n
+comment 9
+pickup ne
+step nw
+step nw
+step w
+step w
+drop
+comment 12
+pickup n
+step ne
+step ne
+step e
+step e
+drop
+step w
+comment 15
+pickup sw
+step e
+step e
+drop
+step s
+step s
+step sw
+pickup s
+step w
+step nw
+drop
+step sw
+pickup s
+step n
+drop
+
+
+DEFINE COMMENT 0
+eJztj7FKgmEYhZ/rUCES/IkGkZAQEUmExhSznwIHkXDRS3A4ODQUpRKCQ0SDky7S0JZTZET9F9DUINEo
+0QX0fv9t5IHDB8853/vyFjEl8OSRZIs9PGraRMR4UIRXInwQ5VMbfJFmRYFfFflx33TMtnwyVKmoxC0l
+5pR5c5mxlE7YoU6WJnla7If8kI5lXcvO1DC36Ya8wjNHBJyyNPYdsiub3SOvHj6X3OmC+3Bvn6Su2dXI
+Zo/JMbWe04BzhvQ15oaZ2XUD67xzYPb1wkQL2+H4o908J64newNSdmWatdb6h/oD5XZBcQ;
+
+DEFINE COMMENT 1
+eJztjbEKgnAchL8n6XlETEWioWzwP/kEDlZ0BUGESOIrODQ3NEiTU1PP1K+3aPh/x3HDcdwM0JwVmWlN
+QEGkgtzSsaFSwqCYp+WoJZMcL36bCylnYg623bKj5q6ah/a8OfKxfuRGq57S7NSxoCHiSqiTfXk8nr/g
+C1PHIdU;
+
+DEFINE COMMENT 2
+eJyTYGBgaEhjEGhIZhBgKGWQY6hn0GJoZzBr6GCwAdIuDU0MPgx1DAENFQwRDVkMCQ2ZDEkMWQxpDHkM
+RQ1lDE0MNQyTgPJzGuoYFjHUMqwDmrGNoYBhB9DMAw2JDAcYRsEoGAWDGgAA/hAZwA;
+
+DEFINE COMMENT 3
+eJztkL1LglEUxn9jYJR9TPZBRIMURDhEOLyEfUFDYwVFmUgNRTUEZUicJQdpCUKoHEP6eBGKkJQmsYi0
+hiiaqr1w6S/ovBf/hiYfeLic557z3OfcMCAuHnBRUGaklhT1bEkD09LEIB7c4qFGWpiQNtbpYJEuxvHS
+Tw9e6aVVzyscn1G6GaNTRlQfYlIZlwA5545Z2mUGiynOTT2vvgs0i+O2jF9WsGSNsL6wzSpHLJEmRIag
+0kGEoGwqI6ruEJJdzeEgqYkO1T2haoKYHJCUfXLKvBzzbHpszXSqc6lKFhufpPFpZXFLwGhnbHBBVC4R
+suxVtJzY5LnmUbK8Gq2om5fokyLDUuBEafaRknq+MMAHftN3p3vcE+VJPd+I8U7c6GX9rzJz8s2Nqb9w
+80kdP8pfGqmiin/GHxxeYAM;
+
+DEFINE COMMENT 4
+eJwTZGBgaGhhcGjoYLBq6GawYOhhsGFoYohoqGXIYqhhaGuoY5jD0MCwiKGLYV3DVIYNDCDQx1DS0MXQ
+AdSziqGF4RbDKBgFo2DIAgChrRND;
+
+DEFINE COMMENT 5
+eJyTZWBgaPBnSGsIZIhjCGaIYUhg0GPIYJBiyGKQaMgDY7uGHIa4hjSGeQypDHsYQKCbQaqhmcGJoYXB
+raGfwY9hElDXJKAZ/Qx9Dd0MSxpaGXZB1Qk19DJIMExkkGGYwaDQsIBBg2ExEI+CUTAKBg0AAKigGyQ;
+
+DEFINE COMMENT 6
+eJztkU1L1GEUxX+UqzZGm4xEzNREWgwqvbiQogiDKQp3LVyEuitMXMgkeFAT0Sk3IimDhKthaiG+/CWd
+hQuTYWAkJAbRZQi6UEL8AB2fPkZeODyX+3LuufcRtovs6gJFI68SUrpEnFKquEyVrtClqyR1jTGuM0gF
+r7lBq7OPqSZGDfWqpY5bNDmWMXK6SfGMlmYquU+d7hJXIyM0MmlE9lfP0npCpR7RpwdkjM0Qe0qZ4mZ5
+5r6XtKiNV7TRzwtSPGfO+QwP/9VayVt1Gl1084ZevSMROIbcO0AH792TYN5vLsQ/eKOP1jlh/RNmG6fD
+td0M0OP6hLdLaoR1jXPguj+hZ9YaZ2lQinZN84kpojB7yTeKzPOdFtacX+W2Ir/zVvrVXGnrTLOgtLVm
+2OIb21ayqUW+GJNaQCwyrCVjmVFW+KwVd0RkWWYnzC74rgWqleeefjDMhm+3wc+Q2/WsPc/+xYyZs0FT
+zjwFksE/4g7H3vXYNzxkLsR++7b7lOvAnCfEdOr/O7f/3v4CAaGICQ;
+
+DEFINE COMMENT 7
+eJztzD0OAWEUheF3IYqRKBQKtU5BaQESIzN6pZCIHGNCRBg/swKxBrEClcISLMWxCs33Jqe6T24V12ai
+DmP1aWhIpJQ6I7okzIm5MuD+Y8qIWHoZFa3tNtS0p8mJlkp6XkxJqqN/bZnaL1hZ5+Q6+HLmxoWHCp7s
+eNm8NeNDKBT6W1/ZgSTF;
+
+DEFINE COMMENT 8
+eJztkLFKwmEUxX+P0BBCQmQWYqIQDUISDQ5NDaG1RISISIEUCZWE2SmkKaIhpExaggYpBMeSiKZSiQiC
+aGpsaGjoAbr96SEaPHA4cL/LPec7c4B66KIXt/rw009YXuYZYB8vV3h4N37xC6/t+XEphEdhxjVKjAgJ
+0xRhMhpmSUEyDJEmQJIgCUIkNcKBxnhUlE9N/N2K2a04AcWZNp4zw5Mz3zGXbXMsGDcYZJmIFpligVml
+KBovlOZOGZ7JGte41Tp15aiY7rKKtELRdI+8+RY41BZVbfKhPN+OxzFuyvg4Iaoy4oi6saESTUq8OjuX
+dFOzDDUmqXLjzCpkVSHHqV0+s6S/eLC8TeujZdrGp7b10jbPFg3n/RoX9+b1Yn99s1Y66OCf4QcHgFqc
+;
+
+DEFINE COMMENT 9
+eJztjDEKwmAUg7+zODo4ioOTOIg6iDhJEap29QSiQYt1sRX5555DxFk8lukpXP4PQniPJC3MhCVjEqYk
+mrFgRY+ULht7xkAZW6W8rI/WfJsKR9o60EHOnOirYKiKEQ+vBOYKXgnsfOe6UXPhyZl3U5XYk/tfcNWd
+UrVTkUjkL/wA0r0iog;
+
+DEFINE COMMENT 10
+eJwTZmBgaMhnUGPIY1BrKGLQa6hnsGtoZ/Bp6GcIY5jIEMUwhSEDSDcx9AJZjQy7GgoYnjCA9JQyTGko
+AeIahkUNXQwbGqYy7GAYBaNgFAw5AABzlRYy;
+
+DEFINE COMMENT 11
+eJyTYWBgaJjDYNIwi0GvYSaDQcMUBqOGLgaHhhaGAIZmhqSGZoaMhnaGOoYJDHMYpjJsY5jOcAaILzVM
+ZnjC0MPwASj/gaGR4V1DFcMDhgqgeAXDKSB7F1BsSUM3QxfDFIYChhkMcUCzHRhmMpgwjIJRMAoGFQAA
+TVklQw;
+
+DEFINE COMMENT 12
+eJwTZgCCUgYhhhIgzmHwachhiGPIY0hqqGbIYKhlyGloYGgB4h6GeoYlDJUMpxpKGS6BtDQUMEBAAVg/
+V8MsBjuGUTAKRsGQAwCWLRCz;
+
+DEFINE COMMENT 13
+eJyTYACCBIa8hgIGB4ZKBp2GegaFhiYGBYZmBi2GegY/oPgmIN7HUMhwCKSUYQuDSMM8hjSGOQxlDbMZ
+ehjmM8xg2MAwDyy3gaGiYR1DDcNahpaG5UA9SxmeMIyCUTAKBjUAAOXSGfU;
+
+DEFINE COMMENT 14
+eJwTYwCCDgYDhlYGDYY6Bo2GCiC7jMGioYghCIgjGMoZ8hqqGPIYGhmSGloYwho6GSwYuoBqOxmUGtoZ
+lID6tIDiZkDxJQ2tDKcYmhhuNNQz3GMYBaNgFAwJAACYdxdz;
+
+DEFINE COMMENT 15
+eJwTY2BgaOhkkGPoYpBi6GCQa2hgMGqoZPBoKGbIYChg6GPIZ9gGpA8xFDNcYihhuNZQwXCHoZrhFkM9
+w4WGFoY1DG0ME4C4p6GZoYahiaGqoQaIC4GmjYJRMAqGBAAAYyIZ9g;
+
