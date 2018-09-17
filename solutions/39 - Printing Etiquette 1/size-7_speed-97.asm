6c7f255b907d5b034ea72ebebad34ad8e7f387d9
Anselm von Wangenheim <anselm.w@gmx.de>
2018-09-18 00:23:09 +0200

    add 39 - Printing Etiquette 1/size-7_speed-97.asm

---
+++ solutions/39 - Printing Etiquette 1/size-7_speed-97.asm
@@ -0,0 +1,20 @@
+-- 7 Billion Humans (2053) --
+-- 39: Printing Etiquette 1 --
+
+-- Author: ansvonwa
+-- Size: 7
+-- Speed: 97
+-- Speed Tests: 98, 96, 97
+
+mem1 = nearest printer
+a:
+if myitem == nothing and
+ mem2 < 5:
+	takefrom mem1
+	mem2 = calc mem2 + 1
+endif
+step nw,w,sw,n,s,ne,e,se
+drop
+jump a
+
+
