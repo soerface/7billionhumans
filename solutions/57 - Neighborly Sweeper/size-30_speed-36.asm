b6eecab741e3bf5fb125965dd64dad42e90bc931
Lucy Phipps <landfillbaby@outlook.com>
2018-09-20 16:31:08 +0100

    56: (8, 13); 57: (9, 982), (30, 36)

---
+++ solutions/57 - Neighborly Sweeper/size-30_speed-36.asm
@@ -0,0 +1,51 @@
+-- 7 Billion Humans (2145) --
+-- 57: Neighborly Sweeper --
+
+-- Author: landfillbaby
+-- Size: 30
+-- Speed: 36
+
+if w == wall:
+	mem4 = set 1
+endif
+a:
+if mem4 == 1:
+	step e
+else:
+	step w
+endif
+if c >= 0:
+	mem1 = set 0
+	mem2 = foreachdir nw,w,sw,n,s,ne,e,se:
+		if mem2 >= 0:
+			if mem1 == 7:
+				mem1 = set 8
+			endif
+			if mem1 == 6:
+				mem1 = set 7
+			endif
+			if mem1 == 5:
+				mem1 = set 6
+			endif
+			if mem1 == 4:
+				mem1 = set 5
+			endif
+			if mem1 == 3:
+				mem1 = set 4
+			endif
+			if mem1 == 2:
+				mem1 = set 3
+			endif
+			if mem1 == 1:
+				mem1 = set 2
+			endif
+			if mem1 == 0:
+				mem1 = set 1
+			endif
+		endif
+	endfor
+	pickup c
+	write mem1
+	drop
+endif
+jump a
