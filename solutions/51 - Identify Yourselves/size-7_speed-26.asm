e577a883531f166dbf4f4abdc699267e2f119e5a
Lucy Phipps <landfillbaby@outlook.com>
2018-09-20 09:54:09 +0100

    51: (6, 44), (7, 26), (20, 16), (40, 6)

---
+++ solutions/51 - Identify Yourselves/size-7_speed-26.asm
@@ -0,0 +1,16 @@
+-- 7 Billion Humans (2145) --
+-- 51: Identify Yourselves --
+
+-- Author: landfillbaby
+-- Size: 7
+-- Speed: 26
+
+a:
+mem1 = calc mem1 + 1
+if w == worker:
+	jump a
+endif
+step s
+pickup c
+write mem1
+drop
