d21043b6384bea225cc1e94b7738f746ed36b612
Lucy Phipps <landfillbaby@outlook.com>
2018-09-28 21:38:22 +0100

    64: (15, 54), (16, 47); 68: (8, 36)

---
+++ solutions/64 - Binary Counter/size-16_speed-47.asm
@@ -0,0 +1,26 @@
+-- 7 Billion Humans (2145) --
+-- 64: Binary Counter --
+
+-- Author: landfillbaby
+-- Size: 16
+-- Speed: 47
+
+step s
+step s
+if c == button:
+	a:
+	tell nw go
+	step n
+	listenfor ok
+	step s
+	jump a
+endif
+pickup n
+b:
+listenfor go
+tell everyone ok
+drop
+listenfor go
+tell w go
+pickup c
+jump b
