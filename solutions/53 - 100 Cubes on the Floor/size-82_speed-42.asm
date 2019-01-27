f6cf013a4076019d86b82dd19056c5fe853fff65
Anselm von Wangenheim <anselm.w@gmx.de>
2019-01-27 23:39:17 +0100

    fast hardcoded solution for 53

---
+++ solutions/53 - 100 Cubes on the Floor/size-82_speed-42.asm
@@ -0,0 +1,102 @@
+-- 7 Billion Humans (2053) --
+-- 53: 100 Cubes on the Floor --
+
+-- Author: ansvonwa
+-- Size: 82
+-- Speed: 42
+
+if ne == wall or
+ w == datacube:
+	if e == wall and
+	 ne == wall:
+		if s == wall:
+			mem1 = set 79
+		else:
+			mem1 = set 39
+		endif
+		step w
+		step w
+		step w
+	else:
+		if n == nothing:
+			if s == wall:
+				mem1 = set 99
+			else:
+				mem1 = set 49
+			endif
+		else:
+			if n == wall:
+				mem1 = set 9
+			else:
+				mem1 = set 19
+			endif
+		endif
+	endif
+else:
+	if sw == worker:
+		if e == wall:
+			mem1 = set 89
+		else:
+			mem1 = set 59
+			step w
+		endif
+	else:
+		if ne != worker:
+			mem1 = set 29
+		else:
+			mem1 = set 69
+		endif
+	endif
+	step w
+endif
+step w
+pickup c
+write mem1
+drop
+step w
+mem1 = calc mem1 - 1
+pickup c
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+step w
+pickup c
+mem1 = calc mem1 - 1
+write mem1
+drop
+
+
