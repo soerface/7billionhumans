3feb0dd56e1a2ad95ffb6cc5a821c7968008fd2e
DeepAQ <i@aq741.com>
2019-01-27 20:51:31 +0800

    add "solutions/38 - Seek and Destroy 3/size-16_speed-31.asm"

---
+++ solutions/38 - Seek and Destroy 3/size-16_speed-31.asm
@@ -0,0 +1,35 @@
+-- 7 Billion Humans (2212:2214M) --
+-- 38: Seek and Destroy 3 --
+
+-- Author: DeepAQ
+-- Size: 16
+-- Speed: 31
+-- Speed Tests: 29, 30, 32, 32, 31, 29, 32
+
+mem2 = nearest hole
+mem3 = nearest shredder
+a:
+step n
+if n == wall:
+	pickup mem1
+	b:
+	mem4 = nearest worker
+	if mem4 == nothing:
+		giveto mem3
+	endif
+	if myitem > mem4 or
+	 myitem == nothing:
+		step mem2
+	endif
+	if myitem == mem4:
+		step w,sw,s,e,se
+	endif
+	jump b
+endif
+if c < mem1 or
+ mem1 != datacube:
+	mem1 = set c
+endif
+jump a
+
+
