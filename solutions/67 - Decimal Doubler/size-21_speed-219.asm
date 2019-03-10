81c99000e354c40241921f32e46d44c1a35bf883
Sören Wegener <wegener92@gmail.com>
2019-03-10 18:17:01 +0100

    Save one command by inverting the if

--- solutions/67 - Decimal Doubler/size-21_speed-219.asm
+++ solutions/67 - Decimal Doubler/size-21_speed-219.asm
@@ -6,30 +6,31 @@
 -- Speed: 219
 
 step s
-if s == button:
-	listenfor go
+if s != button:
+	pickup c
+	mem3 = set myitem
+	step s
+	a:
+	drop
+	step se
+	step nw
+	mem3 = calc mem3 x 2
+	if e >= 5:
+		mem3 = calc mem3 + 1
+	else:
+		mem3 = calc mem3 + 0
+	endif
+	if mem3 >= 10:
+		mem3 = calc mem3 - 10
+	else:
+		mem1 = calc 0 + 0
+	endif
+	pickup c
+	write mem3
+	jump a
 endif
-pickup c
-mem3 = set myitem
-step s
-a:
-drop
-step se
-step nw
-mem3 = calc mem3 x 2
-if e >= 5:
-	mem3 = calc mem3 + 1
-else:
-	mem3 = calc mem3 + 0
-endif
-if mem3 >= 10:
-	mem3 = calc mem3 - 10
-else:
-	mem1 = calc 0 + 0
-endif
-pickup c
-write mem3
-jump a
+
+
 
 
 
