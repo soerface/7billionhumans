36d53daf278ef4f3729bc5cba2f2398d5411bd6d
Sören Wegener <wegener92@gmail.com>
2021-07-03 19:17:18 +0200

    Create size-11_speed-241.asm

---
+++ solutions/61 - Lazy Pathways/size-11_speed-241.asm
@@ -0,0 +1,25 @@
+-- 7 Billion Humans (2145) --
+-- 61: Lazy Pathways --
+
+-- Author: soerface
+-- Size: 11
+-- Speed: 241
+a:
+step nw,w,sw,n,s,e
+if c == datacube and
+ c < 99:
+	mem1 = set c
+	mem2 = calc mem1 + 1
+	mem3 = foreachdir nw,w,sw,n,s,ne,e,se:
+		if mem3 == datacube and
+		 mem3 > mem2:
+			pickup mem3
+			write mem2
+			drop
+			step mem1
+		endif
+	endfor
+endif
+jump a
+
+
