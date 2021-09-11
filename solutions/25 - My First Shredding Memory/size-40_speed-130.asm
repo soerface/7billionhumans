93cc0ada554a6e2a11485a3d6cc986b1ee4e5bc3
Joerg Behrmann <behrmann@physik.fu-berlin.de>
2021-09-11 16:17:08 +0200

    add slightly faster solution to 25

---
+++ solutions/25 - My First Shredding Memory/size-40_speed-130.asm
@@ -0,0 +1,47 @@
+-- 7 Billion Humans (2234) --
+-- 25: My First Shredding Memory --
+
+-- Author: behrmann
+-- Size: 40
+-- Speed: 130
+    
+mem2 = nearest shredder
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
+mem1 = nearest datacube
+pickup mem1
+giveto mem2
