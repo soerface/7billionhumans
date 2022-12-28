d6f5674460b31495a81b9b8ed87536c2b6b52889
Lucy Phipps <landfillbaby@outlook.com>
2022-12-28 17:15:45 +0000

    07 size 18 speed 4
    decided to play this again

---
+++ solutions/07 - Collation Station/size-18_speed-4.asm
@@ -0,0 +1,28 @@
+-- 7 Billion Humans (2234) --
+-- 7: Collation Station --
+
+-- Author: landfillbaby
+-- Size: 18
+-- Speed: 4
+
+step s
+step s
+step s
+if n == datacube:
+	pickup n
+	step s
+else:
+	step s
+	if n == datacube:
+		pickup n
+	else:
+		if c == datacube:
+			pickup c
+		else:
+			pickup s
+		endif
+	endif
+endif
+step s
+step s
+drop
