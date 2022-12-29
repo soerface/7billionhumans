41112b2122cf2b672bf201eb7329611b286300eb
Lucy Phipps <landfillbaby69@gmail.com>
2022-12-29 08:11:02 +0000

    12 size 7 speed 8
    also add .asm to horovtom's size 7 speed 12

---
+++ solutions/12 - Unzip/size-7_speed-8.asm
@@ -0,0 +1,22 @@
+-- 7 Billion Humans (2234) --
+-- 12: Unzip --
+
+-- Author: landfillbaby
+-- Size: 7
+-- Speed: 8
+
+a:
+pickup c
+if nw == wall or
+ se == 0 or
+ sw == 0:
+	step n
+endif
+if se == wall and
+ w == 0 or
+ ne == 0 or
+ nw == 0:
+	step s
+endif
+drop
+jump a
