17615e80d641dc88cdefda1a2cf07acb54ba05c2
David Gauch <davidgauch@gmail.com>
2023-04-13 22:55:01 -0700

    Level 66

---
+++ solutions/66 - Decimal Counter/size-47-speed-88.asm
@@ -0,0 +1,69 @@
+-- 7 Billion Humans (2235) --
+-- 66: Decimal Counter --
+
+-- Author: davidgauch
+-- Size: 47
+-- Speed: 88
+
+step s
+pickup c
+step s
+if myitem != datacube:
+	a:
+	tell nw go
+	step n
+	listenfor ready
+	step s
+	jump a
+endif
+b:
+mem1 = set myitem
+c:
+drop
+listenfor go
+if mem1 < 4:
+	if mem1 < 2:
+		if mem1 < 1:
+			mem1 = set 1
+		else:
+			mem1 = set 2
+		endif
+	else:
+		if mem1 < 3:
+			mem1 = set 3
+		else:
+			mem1 = set 4
+		endif
+	endif
+else:
+	if mem1 < 8:
+		if mem1 < 6:
+			if mem1 < 5:
+				mem1 = set 5
+			else:
+				mem1 = set 6
+			endif
+		else:
+			if mem1 < 7:
+				mem1 = set 7
+			else:
+				mem1 = set 8
+			endif
+		endif
+	else:
+		if mem1 < 9:
+			mem1 = set 9
+		else:
+			tell w go
+			pickup c
+			write 0
+			jump b
+		endif
+	endif
+endif
+pickup c
+write mem1
+tell everyone ready
+jump c
+
+
